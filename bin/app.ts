3c84b1f06a8e951c01a8b55cf99fcff9ed2433c2
jonathanPhan17 <maxandnok@gmail.com>
2026-07-01 23:52:11 -0700

    feat: single-table and resume-bucket constructs, gsi1, cors and app tags

--- bin/app.ts
+++ bin/app.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { App } from 'aws-cdk-lib';
+import { App, Tags } from 'aws-cdk-lib';
 import { loadConfig } from '../lib/config';
 import { DataStack } from '../lib/data-stack';
 import { ApiStack } from '../lib/api-stack';
@@ -7,10 +7,16 @@ import { ApiStack } from '../lib/api-stack';
 const app = new App();
 const config = loadConfig(app);
 
-const data = new DataStack(app, `Coffeegrinder-Data-${config.envName}`, { config });
+Tags.of(app).add('Project', 'coffeegrinder');
+Tags.of(app).add('Environment', config.envName);
+Tags.of(app).add('ManagedBy', 'cdk');
 
-new ApiStack(app, `Coffeegrinder-Api-${config.envName}`, {
+const data = new DataStack(app, `Coffeegrinder-Data-${config.envName}`, { config });
+Tags.of(data).add('Component', 'data');
+
+const api = new ApiStack(app, `Coffeegrinder-Api-${config.envName}`, {
   config,
   table: data.table,
   bucket: data.bucket,
 });
+Tags.of(api).add('Component', 'api');
